9a8383cf1653bdc9fe317fed4060673b3a3a117c
jivanbb <np01cp4s220032@islingtoncollege.edu.np>
2025-03-03 23:38:41 +0545

    admin lte template added

---
+++ application/controllers/Admin.php
@@ -0,0 +1,19 @@
+<?php
+class Admin extends CI_COntroller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index()
+    {
+        $this->load->view('include/header');
+        $this->load->view('include/home');
+        $this->load->view('include/footer'); 
+    }
+    public function category()
+    {
+        echo "This is a category function";
+    }
+}
+?>
---
+++ application/views/include/footer.php
@@ -0,0 +1,52 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?php echo base_url() ?>tools/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo base_url() ?>tools/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url() ?>tools/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo base_url() ?>tools/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url() ?>tools/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="<?php echo base_url() ?>tools/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?php echo base_url() ?>tools/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url() ?>tools/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?php echo base_url() ?>tools/moment/moment.min.js"></script>
+<script src="<?php echo base_url() ?>tools/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?php echo base_url() ?>tools/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="<?php echo base_url() ?>tools/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?php echo base_url() ?>tools/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url() ?>tools/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url() ?>tools/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url() ?>tools/dist/js/pages/dashboard.js"></script>
+</body>
+</html>
